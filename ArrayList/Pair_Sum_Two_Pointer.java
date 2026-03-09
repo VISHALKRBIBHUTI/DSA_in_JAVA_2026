7494f6e39e1d3eeabbe1b8d10a5a07fd2c359f15
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2026-03-09 15:39:35 +0530

    Updated Pair Sum Solution

--- ArrayList/Pair_Sum_Two_Pointer.java
+++ ArrayList/Pair_Sum_Two_Pointer.java
@@ -1,3 +1,43 @@
+import java.util.*;
 public class Pair_Sum_Two_Pointer {
+
+    public static List<Integer> pairSum(ArrayList<Integer>lst , int target){
+
+        ArrayList<Integer> newList = new ArrayList<>();
+        
+        int Lp = 0;
+        int Rp = lst.size()-1;
+
+        while(Lp<Rp){
+
+            if(lst.get(Lp) + lst.get(Rp) == target){
+                newList.add(Lp);
+                newList.add(Rp);
+
+                return newList;
+            }
+            else if(lst.get(Lp) + lst.get(Rp) < target){
+                Lp++;
+            }
+            else{
+                Rp--;
+            }
+        }
+
+        return newList;
+    }
+   public static void main(String[] args) {
+
+    ArrayList<Integer> lst = new ArrayList<>();
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+    lst.add(4);
+    lst.add(5);
+    lst.add(6);
+
+
+    System.out.println(pairSum(lst, 5));
     
+   } 
 }
